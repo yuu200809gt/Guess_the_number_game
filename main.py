8620652ad77880db30120a5d3d12f9972e4d0b32
kunsei <yusei.2989gt@gmail.com>
2025-06-01 13:31:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import random
 
 #ゲームチュートリアル
 print("数字当てゲーム")
-print("1から100までのランダムの数字を当ててください。")
+print("任意の最小値から任意の最大値までのランダムの数字を当ててください。")
 
-target = random.randint(1,100) #1から100までのランダムの数字
+min = int(input("最小値 : "))
+max = int(input("最大値 : "))
+
+target = random.randint(min,max) #1から100までのランダムの数字
 max_tries = 5 #試行回数
 
 #ゲームスタート
 for i in range(1,max_tries + 1):
     try:
-        guess = int(input(f"[{i}回目] 数字を入力してください (1~100) : "))
+        guess = int(input(f"[{i}回目] 数字を入力してください  : "))
         if guess < 1 or guess > 100:
-            print("範囲外です！1〜100の数字を入力してください。\n")
+            print("範囲外です！指定した最小値から最大値の数字を入力してください。\n")
             continue
         if guess == target:
             print(f"正解！{i}回目で当てました！おめでとう！")
